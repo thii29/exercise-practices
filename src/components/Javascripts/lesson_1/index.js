fc657dc4d0ff52e3c5f435a9b09e5419105863d8
Thi Pham <thipham1123@gmail.com>
2024-07-12 21:01:02 +0700

    crud and js

--- src/components/Javascripts/lesson_1/index.js
+++ src/components/Javascripts/lesson_1/index.js
@@ -1,5 +1,5 @@
-// Sửa hàm **joinBrandNames(arr)** 
-//sao cho xuất ra một chuỗi string là danh sách các name của mỗi phần tử trong array 
+// Sửa hàm **joinBrandNames(arr)**
+//sao cho xuất ra một chuỗi string là danh sách các name của mỗi phần tử trong array
 //nối tiếp nhau ngăn cách bởi dấu phẩy và phải có 1 dấu cách sau dấu phẩy
 
 // **Inputs:**
@@ -10,23 +10,22 @@ const arr = [
   { id: 2, name: "Suzuki" },
   { id: 3, name: "Ford" },
 ];
-// **Kết quả mong muốn:** 
-//Một chuỗi string gồm tất cả các name 
-//của các object trong arr chuyền vào, 
+// **Kết quả mong muốn:**
+//Một chuỗi string gồm tất cả các name
+//của các object trong arr chuyền vào,
 //ngăn cách bởi dấu phẩy và một khoảng trắng sau dấu phẩy ", " vd: "Toyota, Suzuki, Ford"
 // => const result = "Toyota, Suzuki, Ford";
 
 // VIẾT HÀM Ở ĐÂY
 
 function joinBrandNames(array) {
-    let newArr = [];
-    for (let index = 0; index < array.length; index++) {
-      newArr = newArr.push(array[index]?.name);
-    }
-    let strName = newArr.join(", ");
-    return strName;
+  let newArr = [];
+  for (let index = 0; index < array.length; index++) {
+    newArr.push(array[index]?.name);
   }
-  
-  let result = joinBrandNames(arr);
-  console.log(result);
+  let strName = newArr.join(", ");
+  return strName;
+}
 
+let result = joinBrandNames(arr);
+console.log(result);
