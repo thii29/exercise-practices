48472e8e4847a5899239a084892f69cfa61687ed
Thi Pham <thipham1123@gmail.com>
2024-07-24 17:51:57 +0700

    add modal-input error

--- src/components/CRUD/CustomerList.tsx
+++ src/components/CRUD/CustomerList.tsx
@@ -11,12 +11,13 @@ type CustomerType = {
   email: string;
 };
 
-
-
 const CustomerList = () => {
   const [customers, setCustomers] = useState<CustomerType[]>(userdata);
-  const [inputName, setInputName] = useState("");
   const [editCustomer, setEditCustomer] = useState<CustomerType | null>(null);
+  const [inputName, setInputName] = useState("");
+  const [inputUserName, setInputUserName] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+
 
   //mo modal insert
   const [open, setOpen] = useState(false);
@@ -41,12 +42,15 @@ const CustomerList = () => {
     const newCustomerObj = {
       id: (Math.random() * stringLength).toString(),
       name: inputName,
-      username: "",
-      email: "abcdefg@gmail.com",
+      username: inputUserName,
+      email: inputEmail,
     };
     const newCustomer = [...customers, newCustomerObj];
     setCustomers(newCustomer);
     setInputName("");
+    setInputUserName("");
+    setInputEmail("");
+    setOpen(!open);
   };
 
   //edit
@@ -88,43 +92,27 @@ const CustomerList = () => {
   };
   return (
     <div>
-      {/* <div className="input-wrap">
-        <div>{editCustomer ? "Edit customer name:" : "Customer name:"}</div>
+      <div className="input-wrap">
         <div>
-          <input
-            type="text"
-            className="input"
-            value={inputName}
-            onChange={(e) => {
-              setInputName(e.target.value);
-            }}
-          />
+          <input type="text" className="input" value={inputName} />
         </div>
         <div>
-          {editCustomer ? (
-            <input
-              type="button"
-              value="Done"
-              className="btn"
-              onClick={editName}
-            />
-          ) : (
-            <input
-              type="button"
-              value="Add"
-              className="btn"
-              onClick={() => {
-                addInfo();
-              }}
-            />
-          )}
+          <input type="button" value="Search" className="btn" />
         </div>
-      </div> */}
-      <InsertCustomer open={open} setOpen={setOpen}/>
+      </div>
+      <InsertCustomer open={open} setOpen={setOpen} 
+                      inputName = {inputName} inputUserName = {inputUserName} 
+                      inputEmail = {inputEmail} addInfo={addInfo}/>
       <div className="list-container flex-column">
         <div className="title-wrapper">
           <h2>Customer List</h2>
-          <button className="btn" onClick={()=>{setOpen(!open)}}>Add new</button>
+          <button
+            className="btn"
+            onClick={() => {
+              setOpen(!open);
+            }} >
+            Add new
+          </button>
         </div>
 
         <div className="list flex-column">
--- src/components/CRUD/Modal/InsertCustomer.tsx
+++ src/components/CRUD/Modal/InsertCustomer.tsx
@@ -5,36 +5,71 @@ import React from "react";
 
 type Props = {
   open: boolean;
-  setOpen: (open:boolean) => void;
-}
+  setOpen: (open: boolean) => void;
+  inputName: string;
+  inputUserName: string;
+  inputEmail: string;
+  addInfo: ()=>void;
+};
 
-const InsertCustomer = ({open, setOpen}: Props) => {
+const InsertCustomer = ({ open, setOpen, inputName, inputUserName, inputEmail, addInfo }: Props) => {
   return (
-    <div className={`modalWrapper ${open? '': 'close'}`}>
+    <div className={`modalWrapper ${open ? "" : "close"}`}>
       <div className="modalBox">
         <div className="header-box">
           <h3>INSERT CUSTOMER</h3>
-          <button className="cancel-icon" onClick={()=>{setOpen(!open)}}>X</button>
+          <button className="cancel-icon" onClick={() => { setOpen(!open);  }} >
+            X
+          </button>
         </div>
         <div className="body-box">
-            <form action="" method="post">
-                <div className="input-box">
-                    <label htmlFor="" className="input-label">User name: </label>
-                    <input className="input" type="text" name="username" id="" value='' placeholder="Enter username"/>
-                </div>
-                <div className="input-box">
-                    <label htmlFor="" className="input-label">Full name: </label>
-                    <input className="input" type="text" name="fullname" id="" value='' placeholder="Enter your full name"/>
-                </div>
-                <div className="input-box">
-                    <label htmlFor="" className="input-label">Email: </label>
-                    <input className="input" type="text" name="email" id="" value='' placeholder="Enter email"/>
-                </div>
-                <div className="form-btn">
-                    <button type="submit" className="btn-cancel">Cancel</button>
-                    <button type="submit" className="btn-ok">OK</button>
-                </div>
-            </form>
+          <div className="input-box">
+            <label htmlFor="" className="input-label">
+              User name:
+            </label>
+            <input
+              className="input"
+              type="text"
+              name="username"
+              id=""
+              value={inputUserName}
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="input-box">
+            <label htmlFor="" className="input-label">
+              Full name:
+            </label>
+            <input
+              className="input"
+              type="text"
+              name="fullname"
+              id=""
+              value={inputName}
+              placeholder="Enter your full name"
+            />
+          </div>
+          <div className="input-box">
+            <label htmlFor="" className="input-label">
+              Email:
+            </label>
+            <input
+              className="input"
+              type="text"
+              name="email"
+              id=""
+              value={inputEmail}
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-btn">
+            <button type="submit" className="btn-cancel" onClick={()=>setOpen(!open)}>
+              Cancel
+            </button>
+            <button type="submit" className="btn-ok" onClick={addInfo}>
+              OK
+            </button>
+          </div>
         </div>
       </div>
     </div>
